25e6bbf02ac47d9b9ae3e6c689bbec06254ef815
Swarali Nikam <nikamswara17@gmail.com>
2025-09-03 18:58:03 +0530

    Add files via upload

---
+++ Virtual Painter (1)/handtrackingmodule.py
@@ -0,0 +1,101 @@
+import cv2
+import mediapipe as mp
+import time
+
+class handDetector():
+    def __init__(self,mode=False,maxHands=2,detectionCon=0.5,trackCon=0.5,):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode,
+            max_num_hands=int(self.maxHands),
+            min_detection_confidence=float(self.detectionCon),
+            min_tracking_confidence=float(self.trackCon)
+        )
+        self.mpDraw = mp.solutions.drawing_utils
+
+        self.tipIds = [4,8,12,16,20]
+
+    def findHands(self, img, draw=True):
+
+      imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+      self.results = self.hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+      if self.results.multi_hand_landmarks:
+         for handLms in self.results.multi_hand_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+      return img
+
+
+    def findPosition(self, img, handNo=0,draw=True):
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+           myhand = self.results.multi_hand_landmarks[handNo]
+           for id, lm in enumerate(myhand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                   cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+
+        if len(self.lmList) == 0:
+            return [0,0,0,0,0]
+
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(1)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 17), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
